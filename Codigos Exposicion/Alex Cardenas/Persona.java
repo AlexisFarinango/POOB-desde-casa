d9cd52707bd6bda3c49a68951acfde468f618a55
AlexisFarinango <133933591+AlexisFarinango@users.noreply.github.com>
2024-01-04 21:58:27 -0500

    Archivos de Exposición

---
+++ Codigos Exposicion/Alex Cardenas/Persona.java
@@ -0,0 +1,11 @@
+public class Persona {
+    public String Nombre;
+    public int Edad;
+
+    public Persona(String Nombre,int Edad) {
+        this.Nombre = Nombre;
+        this.Edad = Edad;
+    }
+}
+
+
